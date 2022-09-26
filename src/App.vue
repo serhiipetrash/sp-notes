5c374ea45df92c2c7b691dfb9bace0911be6b36d
Serhii Petrash <serhiipetrash@gmail.com>
2022-09-26 13:16:25 +0300

    add Bulma to project

--- src/App.vue
+++ src/App.vue
@@ -5,11 +5,15 @@ import { RouterLink, RouterView } from 'vue-router'
 
 <template>
 
-  <RouterLink to="/">Home</RouterLink>
-  <RouterLink to="/about">About</RouterLink>
+  <RouterLink to="/" class="button">Home</RouterLink>
+  <RouterLink to="/about" class="button">About</RouterLink>
 
   <RouterView />
 
 </template>
+
+<style>
+/* @import 'bulma/css/bulma.min.css'; */
+</style>
 
 
