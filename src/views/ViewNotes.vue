87c926f3c0aca915a4e6de1202427be3266505f4
Serhii Petrash <serhiipetrash@gmail.com>
2022-09-27 19:25:37 +0300

    add delete Notes method

--- src/views/ViewNotes.vue
+++ src/views/ViewNotes.vue
@@ -3,86 +3,76 @@
     <!-- form text area -->
     <div class="has-background-success-dark p-4 mb-5">
       <div class="field">
-      <div class="control">
-        <textarea
-          v-model="newNote"
-          class="textarea"
-          placeholder="Add new note"
-          ref="newNoteRef"
-          ></textarea
-        >
-      </div>
-    </div>
-
-    <div class="field is-grouped is-grouped-right">
-      <div class="control">
-        <button 
-        @click="addNote" 
-        :disabled="!newNote" 
-        class="button is-link has-background-success">
-          Add New Note
-        </button>
+        <div class="control">
+          <textarea
+            v-model="newNote"
+            class="textarea"
+            placeholder="Add new note"
+            ref="newNoteRef" 
+          />
+        </div>
       </div>
 
-    </div>
+      <div class="field is-grouped is-grouped-right">
+        <div class="control">
+          <button :disabled="!newNote" @click="addNote" class="button is-link has-background-success">Add New Note</button >
+        </div>
+      </div>
     </div>
 
     <!-- notes -->
-    <div
+    <Note
       v-for="note in notes"
       :key="note.id"
-      class="card mb-4"
-    >
+      :note="note"
+      @deleteClicked="deleteNote"
+    />
 
-      <div class="card-content">
-        <div class="content">
-          {{ note.content }}
-        </div>
-      </div>
-      <footer class="card-footer">
-        <a href="#" class="card-footer-item">Save</a>
-        <a href="#" class="card-footer-item">Edit</a>
-        <a href="#" class="card-footer-item">Delete</a>
-      </footer>
-    </div>
   </div>
 </template>
 
 <script setup>
 import { ref } from 'vue';
+import Note from '../components/Notes/Note.vue';
 
-  const newNote = ref('')
-  const newNoteRef = ref(null)
 
-  const notes = ref([
-    {
-      id: 'id1',
-      content: ' Lorem ipsum dolor sit, amet consectetur adipisicing Lorem ipsum dolor sit, amet consectetur adipisicing Lorem ipsum dolor sit, amet consectetur adipisicing Lorem ipsum dolor sit, amet consectetur adipisicing Lorem ipsum dolor sit, amet consectetur adipisicing '
-    },
-    {
-      id: 'id2',
-      content: 'repellendus veniam nisi nostrum maiores hic debitis sit beatae, nulla deleniti atque quidem sapiente numquam repellendus veniam nisi nostrum maiores hic debitis sit beatae, nulla deleniti atque quidem sapiente numquam repellendus veniam nisi nostrum maiores hic debitis sit beatae, nulla deleniti atque quidem sapiente numquam'
-    },
-    {
-      id: 'id3',
-      content: 'This is a my shorter note! Yea!'
-    }
-  ])
+const newNote = ref('')
+const newNoteRef = ref(null)
 
-  const addNote = () => {
-    let currentDate = new Date().getTime()
-    let id = currentDate.toString()
-
-    let note = {
-      id: id,
-      content: newNote.value 
-    }
-
-    notes.value.unshift(note)
-
-    newNote.value = ''
-
-    newNoteRef.value.focus()
+const notes = ref([
+  {
+    id: 'id1',
+    content: ' Lorem ipsum dolor sit, amet consectetur adipisicing Lorem ipsum dolor sit, amet consectetur adipisicing Lorem ipsum dolor sit, amet consectetur adipisicing Lorem ipsum dolor sit, amet consectetur adipisicing Lorem ipsum dolor sit, amet consectetur adipisicing '
+  },
+  {
+    id: 'id2',
+    content: 'repellendus veniam nisi nostrum maiores hic debitis sit beatae, nulla deleniti atque quidem sapiente numquam repellendus veniam nisi nostrum maiores hic debitis sit beatae, nulla deleniti atque quidem sapiente numquam repellendus veniam nisi nostrum maiores hic debitis sit beatae, nulla deleniti atque quidem sapiente numquam'
+  },
+  {
+    id: 'id3',
+    content: 'This is a my shorter note! Yea!'
   }
+])
+
+const addNote = () => {
+  let currentDate = new Date().getTime()
+  let id = currentDate.toString()
+
+  let note = {
+    id: id,
+    content: newNote.value
+  }
+
+  notes.value.unshift(note)
+
+  newNote.value = ''
+
+  newNoteRef.value.focus()
+}
+
+const deleteNote = (idToDelete) => {
+  // console.log('deleteNote', idToDelete);
+  notes.value = notes.value.filter((note) => { return note.id !== idToDelete })
+}
 
 </script>
