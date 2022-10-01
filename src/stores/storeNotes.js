5d473ec590d75096c89f3f297717b42c05825ef2
Serhii Petrash <serhiipetrash@gmail.com>
2022-10-01 21:04:35 +0300

    get notes in real time

--- src/stores/storeNotes.js
+++ src/stores/storeNotes.js
@@ -1,5 +1,5 @@
 import { defineStore } from 'pinia'
-import { collection, getDocs } from 'firebase/firestore'
+import { collection, onSnapshot } from 'firebase/firestore'
 import { db } from '@/js/firebase'
 
 export const useStoreNotes = defineStore('storeNotes', {
@@ -23,14 +23,19 @@ export const useStoreNotes = defineStore('storeNotes', {
   },
   actions: {
     async getNotes() {
-      const querySnapshot = await getDocs(collection(db, 'notes'));
-      querySnapshot.forEach((doc) => {
+
+      onSnapshot(collection(db, 'notes'), (querySnapshot) => {
+        let notes = []
+        querySnapshot.forEach((doc) => {
         let note = {
           id: doc.id,
           content: doc.data().content
         }
-        this.notes.push(note)
+        notes.push(note)
+        })
+        this.notes = notes
       })
+      
     },
     addNote(newNoteContent) {
       let currentDate = new Date().getTime()
