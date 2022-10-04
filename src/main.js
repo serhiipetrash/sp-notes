2518513691717b796109c008fd590552e2b8d7ff
Serhii Petrash <serhiipetrash@gmail.com>
2022-10-04 13:34:25 +0300

    redirect user on Auth

--- src/main.js
+++ src/main.js
@@ -1,6 +1,5 @@
-import { createApp } from 'vue'
+import { createApp, markRaw } from 'vue'
 import { createPinia } from 'pinia'
-
 import App from './App.vue'
 import router from './router'
 
@@ -8,7 +7,11 @@ import './assets/main.css'
 
 const app = createApp(App)
 
-app.use(createPinia())
-app.use(router)
+const pinia = createPinia()
+pinia.use(({ store }) => {
+  store.router = markRaw(router)
+})
 
+app.use(pinia)
+app.use(router)
 app.mount('#app')
