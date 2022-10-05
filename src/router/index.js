f2a65e68f41e296e515dbc2ce86b30c5a4e53d60
Serhii Petrash <serhiipetrash@gmail.com>
2022-10-05 13:53:29 +0300

    add Navigation guards in router

--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,17 @@
 import { createRouter, createWebHistory } from 'vue-router'
+import { useStoreAuth } from '@/stores/storeAuth'
 import ViewNotes from '../views/ViewNotes.vue'
 import ViewEditNote from '../views/ViewEditNote.vue'
 import ViewStats from '../views/ViewStats.vue'
 import ViewAuth from '../views/ViewAuth.vue'
+import { auth } from '../js/firebase'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/',
-      name: 'home',
+      name: 'notes',
       component: ViewNotes
     },
     {
@@ -28,6 +30,17 @@ const router = createRouter({
       component: ViewAuth
     },
   ]
+})
+// navigation guards
+router.beforeEach(async (to, from) => {
+  const storeAuth = useStoreAuth()
+  console.log('to: ', to)
+  if(!storeAuth.user.id && to.name !== 'auth') {
+    return { name: 'auth' }
+  }
+  if(storeAuth.user.id && to.name === 'auth') {
+    return false
+  }
 })
 
 export default router
