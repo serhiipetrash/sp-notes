9b7ab59831bdbdb8d4133d9a68e19afcde38482a
Serhii Petrash <serhiipetrash@gmail.com>
2022-10-08 18:33:06 +0300

    refactoring code, some fix

--- src/router/index.js
+++ src/router/index.js
@@ -4,7 +4,7 @@ import ViewNotes from '../views/ViewNotes.vue'
 import ViewEditNote from '../views/ViewEditNote.vue'
 import ViewStats from '../views/ViewStats.vue'
 import ViewAuth from '../views/ViewAuth.vue'
-import { auth } from '../js/firebase'
+
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
